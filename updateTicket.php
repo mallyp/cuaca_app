a6379c6866c040926cd94e2ee1f86fe977f97fde
mallyp <102567864+mallyp@users.noreply.github.com>
2023-07-12 05:00:17 +0800

    Update updateTicket.php

--- updateTicket.php
+++ updateTicket.php
@@ -41,6 +41,56 @@ if (isset($_GET['id'])) {
 }
 ?>
 
+
+<style>
+body {
+    background-image: url("https://images.unsplash.com/photo-1528458909336-e7a0adfed0a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWVzdGhldGljJTIwbmV3c3BhcGVyJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60");
+}
+    form {
+        margin-top: 20px;
+        font-family: Arial, sans-serif;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 10px;
+    }
+
+    textarea {
+        padding: 10px;
+        border: 1px solid #E5E5E5;
+        width: 400px;
+        max-width: 100%;
+        color: #999999;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+    }
+
+    input[type="submit"] {
+        margin-top: 10px;
+        background-color: #474E69;
+        color: #FFF;
+        border-radius: 3px;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+
+    .go-back {
+        display: inline-block;
+        margin-top: 10px;
+        background-color: #AB0341;
+        color: #FFF;
+        border: none;
+        padding: 10px 20px;
+        text-decoration: none;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+
+    .go-back:hover {
+        background-color: #89022e;
+    }
+</style>
+
 <!-- HTML form to update the ticket comment -->
 <form action="" method="POST">
     <label for="comment">Ticket Comment:</label>
@@ -48,3 +98,6 @@ if (isset($_GET['id'])) {
     <br>
     <input type="submit" name="submit" value="Update">
 </form>
+
+<!-- Go back option -->
+<a href="readTicket.php"" class="go-back">Go Back</a>
